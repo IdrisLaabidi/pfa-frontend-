4d73022d9b4a8076993ae01f48bc022d3bf543a1
RayenMiri <rayenmiri000@gmail.com>
2024-02-28 18:14:16 +0100

    test1

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 function App() {
   return (
     <div className="App">
-      
+      test 1234
     </div>
   );
 }
---
+++ src/Pages/trialpage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const trialpage = () => {
+  return (
+    <div>
+        123123123
+    </div>
+  )
+}
+
+export default trialpage;
