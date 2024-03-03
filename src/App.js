f4761ba06bbbb20aa44627ce721c5bcee2b541d0
IdrisLaabidi <idrislaabidi@gmail.com>
2024-03-03 16:45:08 +0100

    started login interface

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import { BrowserRouter , Routes, Route } from "react-router-dom"; // import react router dom
+//import pages 
+import HomePage from "./pages/dashboard/dashboardPage";
+import LoginPage from "./pages/login/loginPage";
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<LoginPage />}/>
+          <Route path="/Home" element={ <HomePage/> } />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
