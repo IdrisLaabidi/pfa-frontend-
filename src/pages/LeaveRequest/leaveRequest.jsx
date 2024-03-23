c7fe434558d59ceaa66ad14437e3639aa18a4324
tasnimebbk <tasnime@gmail.com>
2024-03-24 00:57:57 +0100

    leave request fetchingg

--- src/pages/LeaveRequest/leaveRequest.jsx
+++ src/pages/LeaveRequest/leaveRequest.jsx
@@ -3,25 +3,46 @@ import useFetch from '../../hooks/useFetch';
 import Spinner from '../../components/spinner/spinner';
 import styles from './leaveRequest.css';
 import LoadingModal from '../../components/loadingModal/LoadingModal';
+import Cookies from 'js-cookie';
 
 const LeaveRequest = () => {
- 
+const token=Cookies.get("token");
 const { data: leaves, isPending, error } = useFetch('http://localhost:4000/api/leave/leaves/');
+const userIdArray = leaves.map((leave) => leave.concernedUser) || [];
+const [user, setUser] = useState(null);
+const fetching =async()=>{
+  try {
+      
+    const response = fetch('http://localhost:4000/api/auth/Leaveusers', {
+      method: 'GET',
+      headers: {
+        'Authorization': `Bearer ${token}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({userIdArray})
+    });
 
- const userIdArray = leaves?.map((leave) => leave.concernedUser) || [];
+    if (!response.ok) {
+      throw new Error('Failed to fetch user data');
+    }
 
-  const FetchUser = (userId) => {
-    const { data, isPending, error } = useFetch('http://localhost:4000/api/auth/users/${userId}');
-    return { userData: data, isLoading: isPending, userError: error };
-  };
-
-  const users = userIdArray.map(userId => FetchUser(userId));
+    const user = await response.json();
+    setUser(user);
+    console.log(user);
+    
+  } catch (error) {
+    console.error('Error fetching user data:', error);
+  }
+  
+}
+   
+    
 
   return (
     <div className={styles.allLeaves}>
       {error && <div>{error}</div>}
       {isPending && <Spinner />}
-      {users && (
+      {user && (
         <table className={styles.Table}>
           <thead>
             <tr>
@@ -30,10 +51,10 @@ const { data: leaves, isPending, error } = useFetch('http://localhost:4000/api/l
             </tr>
           </thead>
           <tbody>
-            {users.map(user => 
-              <tr key={user.id}>
-                <td>{user.firstName} {user.lastName}</td>
-                <td>{user.email}</td>
+            {user?.map(user1 => 
+              <tr key={user1.id}>
+                <td>{user1.firstName} {user1.lastName}</td>
+                <td>{user1.email}</td>
               </tr>
             )}
           </tbody>
