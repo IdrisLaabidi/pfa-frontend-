4e4b7d1b9d229c64c5c34ec3eccedd508c532d54
IdrisLaabidi <idrislaabidi@gmail.com>
2024-03-25 13:20:20 +0100

    completed leavereq styling

--- src/pages/LeaveRequest/leaveRequest.jsx
+++ src/pages/LeaveRequest/leaveRequest.jsx
@@ -1,17 +1,21 @@
 
 import styles1 from '../../components/project/project.module.css'
+import styles from './leaveRequest.module.css'
 
 import { CompactTable } from '@table-library/react-table-library/compact';
 import { BeatLoader } from "react-spinners";
+import ProgressBar from '@ramonak/react-progress-bar';
+import LoadingModal from '../../components/loadingModal/LoadingModal';
+
+import fromIcon from '../../assets/form-icon.svg'
+
 import { useTheme } from '@table-library/react-table-library/theme';
 import { DEFAULT_OPTIONS, getTheme } from '@table-library/react-table-library/material-ui';
 import React, { useEffect, useState } from 'react';
 import useFetch from '../../hooks/useFetch';
 import { format } from 'date-fns';
-
-
-import LoadingModal from '../../components/loadingModal/LoadingModal';
 import Cookies from 'js-cookie';
+
 
 
 const LeaveRequest = () => {
@@ -26,19 +30,32 @@ const LeaveRequest = () => {
   }
  
   const materialTheme = getTheme(DEFAULT_OPTIONS);
-  const theme = useTheme(materialTheme);
+  const theme = useTheme([materialTheme , {
+    Table: `
+      --data-table-library_grid-template-columns:  20% 32% 10% 30% 8%;
+    `,
+}]);
 
   const columns = [
     { label: 'Name', renderCell: (leave) => `${leave.concernedUser.firstName} ${leave.concernedUser.lastName}` },
     { label: 'Email', renderCell: (leave) => leave.concernedUser.email },
-    {label: 'Request Date' , renderCell: (leave)=>format(new Date(leave.createdAt), 'dd/MM/yyyy') }
+    {label: 'Request Date' , renderCell: (leave)=>format(new Date(leave.createdAt), 'dd/MM/yyyy') },
+    {label :'leave Count' , renderCell :(leave => <ProgressBar 
+        completed={leave.concernedUser.leaveCount.toString()} 
+        maxCompleted="90"
+        bgColor='#08639c'
+        ></ProgressBar>)},
+    {label : 'details' , renderCell : leave => <button title='show details' className={styles.buttn}>
+      <img className={styles.icon} alt='details' src={fromIcon}></img>
+    </button>}
     
   ];
 
   return (
-    <div className={styles1.container}>
-      {data && <CompactTable columns={columns} data={data} theme={theme}/>}
+    <div className={styles.container}>
+      {data && <CompactTable columns={columns} data={data} theme={theme} layout={{ fixedHeader: true , custom :true}}/>}
       {isPending && <BeatLoader color="#ffffff"></BeatLoader>}
+      
       <LoadingModal open={data === null} />
     </div>
   );
